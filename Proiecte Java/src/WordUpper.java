adab08fc475f7620013dc8e008858de6162ae737
Bogdan-Cpp <bogdan1000leitan2007@gmail.com>
2024-10-22 22:09:53 +0300

    wordupper change

--- Proiecte Java/src/WordUpper.java
+++ Proiecte Java/src/WordUpper.java
@@ -1,29 +1,24 @@
 public class WordUpper {
 
     public static String wordUpper(String text){
-        String word = "";
+        StringBuilder word = new StringBuilder();
         for(int i = 0; i < text.length(); i++){
-
             if(i % 2 == 0){
-
-                word += Character.toUpperCase(text.charAt(i));
+                word.append(Character.toUpperCase(text.charAt(i)));
             }
 
             else{
-
-                word += text.charAt(i);
+                word.append(text.charAt(i));
             }
 
             if(text.charAt(i) == ' '){
-
-                word += text.charAt(i);
+                word.append(text.charAt(i));
             }
         }
-        return word;
+        return word.toString();
     }
 
     public static void main(String[] args){
-
       String text = "java este un limbaj fain!";
       String word = wordUpper(text);
       System.out.println(word);
